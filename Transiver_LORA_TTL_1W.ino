816baf8dde6be81f92cbae4d6ceaafe1f67ea60d
AlexanderSemenov1 <38571636+AlexanderSemenov1@users.noreply.github.com>
2018-04-22 01:56:21 +0400

    Add files via upload

---
+++ Transiver_LORA_TTL_1W.ino
@@ -0,0 +1,41 @@
+ //Трансивер на передачу показаний
+#include <SoftwareSerial.h>
+#define M0 12
+#define M1 11
+#define SOFT_TX 10
+#define SOFT_RX 9
+#define AUX 8
+
+
+SoftwareSerial softSerial(SOFT_RX, SOFT_TX); // RX, TX
+
+void setup() {
+  
+  pinMode(M0, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(SOFT_RX, INPUT);
+  pinMode(SOFT_TX, OUTPUT);
+  pinMode(AUX, INPUT);
+  
+ Serial.begin(9600);
+ softSerial.begin(9600);  //  скорости передачи 9600 , оптимально
+
+
+ 
+  //digitalWrite(M0, HIGH);   //Здесь можно задать режим работы 
+   digitalWrite(M0, LOW);     // Высокий уравень на М0 и М1 передача всегда включена
+  //digitalWrite(M1, HIGH);   //  Низкий уравень на М0 и М1 передатчик отключен и включается только на время передачи (энергосбережение)
+  digitalWrite(M1, LOW);      //  
+}
+
+void loop() {
+
+  softSerial.println (1234567890  ); // Здесь то что будет передаватся
+  delay(2000);
+  //Serial.println ("hello world");
+  softSerial.println ("Hello World"  );
+ delay(2000);
+ // softSerial.println (0);
+  //delay(2000);
+
+}
